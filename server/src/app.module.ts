4587353edf01676d4bda3b04ea34be056803788f
kkpanfilov <kkpanfilov@outlook.com>
2026-07-16 12:24:48 +0300

    feat(redis): import redis module in app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,30 +1,19 @@
-import { CacheModule } from "@nestjs/cache-manager";
 import { MiddlewareConsumer, Module } from "@nestjs/common";
 import { NestModule } from "@nestjs/common";
-
-import KeyvRedis from "@keyv/redis";
 
 import { AuthModule } from "./auth/auth.module.js";
 import { UserMiddleware } from "./auth/middlewares/user.middleware.js";
 import { CommentsModule } from "./comments/comments.module.js";
 import { PastesModule } from "./pastes/pastes.module.js";
 import { PrismaService } from "./prisma/prisma.service.js";
+import { RedisModule } from "./redis/redis.module.js";
 import { LastActiveMiddleware } from "./users/middlewares/last-active.middleware.js";
 import { UsersModule } from "./users/users.module.js";
 
 // TODO: implement configModule
+// TODO: make docker dev start only redis and postgres
 @Module({
-  imports: [
-    AuthModule,
-    UsersModule,
-    PastesModule,
-    CommentsModule,
-    CacheModule.register({
-      isGlobal: true,
-      stores: [new KeyvRedis(process.env.REDIS_URL as string)],
-      ttl: Number(process.env.CACHE_TTL ?? 60) * 1000,
-    }),
-  ],
+  imports: [RedisModule, AuthModule, UsersModule, PastesModule, CommentsModule],
   controllers: [],
   providers: [PrismaService],
 })
