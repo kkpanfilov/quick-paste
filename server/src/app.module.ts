ac33e318b2c0524af8d50e4b5c54810b54ba2970
kkpanfilov <kkpanfilov@outlook.com>
2026-07-17 14:15:03 +0300

    refactor(server): implement config module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,19 +1,33 @@
 import { MiddlewareConsumer, Module } from "@nestjs/common";
 import { NestModule } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 
 import { AuthModule } from "./auth/auth.module.js";
 import { UserMiddleware } from "./auth/middlewares/user.middleware.js";
 import { CommentsModule } from "./comments/comments.module.js";
+import { envSchema } from "./config/env-schema.js";
 import { PastesModule } from "./pastes/pastes.module.js";
 import { PrismaService } from "./prisma/prisma.service.js";
 import { RedisModule } from "./redis/redis.module.js";
 import { LastActiveMiddleware } from "./users/middlewares/last-active.middleware.js";
 import { UsersModule } from "./users/users.module.js";
 
-// TODO: implement configModule
-// TODO: make docker dev start only redis and postgres
+const isProduction = process.env.NODE_ENV === "production";
+
 @Module({
-  imports: [RedisModule, AuthModule, UsersModule, PastesModule, CommentsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: isProduction ? undefined : "../.env.dev",
+      ignoreEnvFile: isProduction,
+      validate: (config) => envSchema.parse(config),
+    }),
+    RedisModule,
+    AuthModule,
+    UsersModule,
+    PastesModule,
+    CommentsModule,
+  ],
   controllers: [],
   providers: [PrismaService],
 })
