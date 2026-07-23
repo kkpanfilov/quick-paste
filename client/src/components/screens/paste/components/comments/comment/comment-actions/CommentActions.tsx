78707f2617bcdea87570040b8670ffb048821a97
kkpanfilov <kkpanfilov@outlook.com>
2026-07-23 12:53:59 +0300

    refactor(ui): move reply form from CommentActions to separated component

--- client/src/components/screens/paste/components/comments/comment/comment-actions/CommentActions.tsx
+++ client/src/components/screens/paste/components/comments/comment/comment-actions/CommentActions.tsx
@@ -1,32 +1,10 @@
 import { useState } from "react";
 
-import { type InfiniteData, useQueryClient } from "@tanstack/react-query";
-import { type SubmitHandler, useForm } from "react-hook-form";
-
-import { isApiError } from "@/api/apiClient.ts";
 import type { ReplyState } from "@/components/screens/paste/components/comments/comment/Comment.tsx";
+import { ReplyForm } from "@/components/screens/paste/components/comments/comment/comment-actions/reply-form/ReplyForm.tsx";
 import { Button } from "@/components/ui/button/Button.tsx";
-import { ErrorMessage } from "@/components/ui/error-message/ErrorMessage.tsx";
-import { Field } from "@/components/ui/field/Field.tsx";
-import { useCreateReply } from "@/hooks/comments/useCreateReply.ts";
-import type {
-  Cursor as CommentCursor,
-  GetCommentsResult,
-} from "@/hooks/comments/useGetComments.ts";
-import type {
-  GetRepliesResult,
-  Cursor as ReplyCursor,
-} from "@/hooks/comments/useGetReplies.ts";
-import { useNotifications } from "@/hooks/useNotifications.ts";
-import type { CreateReplyDto } from "@/types/reply.types.ts";
 
 import styles from "./CommentActions.module.scss";
-
-type FormData = Omit<CreateReplyDto, "pasteId">;
-
-const DEFAULT_VALUES = {
-  content: "",
-};
 
 type Props = {
   isAuth: boolean;
@@ -35,16 +13,6 @@ type Props = {
 };
 
 export const CommentActions = ({ isAuth, commentId, pasteId }: Props) => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<FormData>({
-    mode: "onSubmit",
-    defaultValues: DEFAULT_VALUES,
-  });
-
   const [replyState, setReplyState] = useState<ReplyState>({
     isReplying: false,
     commentId: null,
@@ -52,74 +20,6 @@ export const CommentActions = ({ isAuth, commentId, pasteId }: Props) => {
 
   const isReplying =
     replyState.isReplying && replyState.commentId === commentId;
-  const { notifySuccess, notifyError } = useNotifications();
-
-  const { mutateAsync: createReply } = useCreateReply();
-  const queryClient = useQueryClient();
-
-  const onReply: SubmitHandler<FormData> = async (body) => {
-    try {
-      const result = await createReply({
-        id: commentId,
-        pasteId,
-        data: { ...body, pasteId },
-      });
-
-      if (result.id) {
-        notifySuccess({
-          title: "Comment added",
-          message: "Comment has been added successfully",
-        });
-
-        queryClient.setQueryData<InfiniteData<GetRepliesResult, ReplyCursor>>(
-          ["comments-replies", commentId],
-          (oldData) => {
-            if (!oldData || !oldData.pages[0]) return oldData;
-
-            return {
-              ...oldData,
-              pages: [
-                {
-                  ...oldData.pages[0],
-                  items: [result, ...oldData.pages[0].items],
-                },
-                ...oldData.pages.slice(1),
-              ],
-            };
-          },
-        );
-
-        queryClient.setQueryData<
-          InfiniteData<GetCommentsResult, CommentCursor>
-        >(["paste-comments", pasteId], (oldData) => {
-          if (!oldData || !oldData.pages[0]) return oldData;
-
-          return {
-            ...oldData,
-            pages: oldData.pages.map((page) => ({
-              ...page,
-              items: page.items.map((comment) =>
-                comment.id === commentId
-                  ? {
-                      ...comment,
-                      repliesCount: comment.repliesCount + 1,
-                    }
-                  : comment,
-              ),
-            })),
-          };
-        });
-
-        setReplyState({ isReplying: false, commentId: null });
-        reset();
-      }
-    } catch (error) {
-      notifyError({
-        title: "Reply not added",
-        message: isApiError(error) ? error.message : "Unknown error",
-      });
-    }
-  };
 
   return (
     <>
@@ -143,50 +43,11 @@ export const CommentActions = ({ isAuth, commentId, pasteId }: Props) => {
       )}
 
       {isAuth && isReplying && (
-        <form
-          id={`reply-form-${commentId}`}
-          className={styles.replyForm}
-          aria-label="Reply to comment"
-        >
-          <Field
-            tag="textarea"
-            className={styles.replyTextarea}
-            placeholder="Write a reply..."
-            aria-label="Write a reply"
-            rows={3}
-            {...register("content", {
-              required: "Content is required",
-              maxLength: {
-                value: 1000,
-                message: "Reply is too long",
-              },
-            })}
-          />
-          <div className={styles.replyFormActions}>
-            {errors.content && (
-              <ErrorMessage message={errors.content.message} />
-            )}
-            <Button
-              variant="ghost"
-              className={styles.replyCancelButton}
-              onClick={() =>
-                setReplyState({
-                  isReplying: false,
-                  commentId: null,
-                })
-              }
-            >
-              Cancel
-            </Button>
-            <Button
-              variant="primary"
-              className={styles.replySubmitButton}
-              onClick={handleSubmit(onReply)}
-            >
-              Send reply
-            </Button>
-          </div>
-        </form>
+        <ReplyForm
+          commentId={commentId}
+          pasteId={pasteId}
+          closeForm={() => setReplyState({ isReplying: false, commentId: null })}
+        />
       )}
     </>
   );
