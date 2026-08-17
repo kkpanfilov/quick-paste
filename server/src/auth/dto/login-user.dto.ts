084cf2609d30278ed1878e029fccbf2739a0595c
kkpanfilov <kkpanfilov@outlook.com>
2026-08-17 14:47:31 +0300

    refactor(auth): align LoginUserDto and RegisterUserDto

--- server/src/auth/dto/login-user.dto.ts
+++ server/src/auth/dto/login-user.dto.ts
@@ -6,14 +6,17 @@ export class LoginUserDto {
   @Transform(({ value }): string =>
     typeof value === "string" ? value.trim() : value,
   )
+  @Length(6, 50, {
+    message: "Email must be at least 6 characters long and no more than 50",
+  })
   @IsEmail({}, { message: "Email is not valid" })
   email!: string;
 
   @IsString({ message: "Password must be a string" })
-  @Transform(({ value }): string =>
-    typeof value === "string" ? value.trim() : value,
-  )
-  @Length(8, 20, { message: "Password must be at least 6 characters long" })
+  @Length(8, 64, {
+    message:
+      "Password must be at least 8 characters long and no more than 64 characters",
+  })
   password!: string;
 
   @IsBoolean({ message: "Remember must be a boolean" })
